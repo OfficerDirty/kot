255eb18c6ea3b22c5617d0ef5e9a39c89cdcdaed
OfficerDirty <a.steding@hotmail.com>
2016-11-28 11:24:13 +0100

    kot

---
+++ src/Model/Card.java
@@ -0,0 +1,15 @@
+package Model;
+
+/**
+ * Created by gast09 on 28.11.2016.
+ */
+public class Card {
+
+    String value;
+    String sign;
+
+    public Card(String value, String sign){
+        this.value = value;
+        this.sign = sign;
+    }
+}
